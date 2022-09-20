e411d24d5a07500c45f103aa13621255a15e55ca
SergioFerreiraFilho <106404822+SergioFerreiraFilho@users.noreply.github.com>
2022-09-20 17:23:16 -0300

    Adicionando a ultima questão que faltava

--- Avaliação Repetição/Questão 2/eventos.js
+++ Avaliação Repetição/Questão 2/eventos.js
@@ -2,3 +2,19 @@
 //a. A menor altura do grupo; 
 //b. A maior altura do grupo;
 
+let arrayAltura = [10,22,11,33,11,0,2,3,1,220,11,0]
+let alturaMaxima = 0
+let alturaMinima = 2222222222222222;
+for (let index = 0; index < arrayAltura.length; index++) {
+    let alturaAtual = arrayAltura [index]
+    console.log('index', index)
+    console.log('index', alturaAtual)
+    console.log('index', alturaMaxima)
+    if (alturaAtual > alturaMaxima ) {
+        alturaMaxima= alturaAtual
+    }
+    if(alturaAtual < alturaMinima)
+    alturaMinima = alturaAtual
+}
+console.log(alturaMaxima)
+console.log(alturaMinima)
